0514c01ec74980bca713f7ff1a34ad38f677da6d
TashwillAndries <tashwilla27@gmail.com>
2021-11-24 08:58:07 +0200

    new updates

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,46 +1,38 @@
-import axios from "axios";
+// import axios from "axios";
 
-export default {
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {
-    login: ({ commit }, { email, password }) => {
-      return new Promise((resolve, reject) => {
-        axios
-          .post(
-            "api login route goes here",
-            { email, password },
-            { withCredentials: true }
-          )
-          .then(({ data, status }) => {
-            if (status === 200) {
-              resolve(true);
-            }
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      });
-    },
-    register: ({ commit }, { username, email, password }) => {
-      return new Promise((resolve, reject) => {
-        axios
-          .post(
-            "api register router goes here",
-            {
-              username,
-              email,
-              password,
-            },
-            { withCredentials: true }
-          )
-          .then(({ data, status }) => {
-            if (status === 201) {
-              resolve(true);
-            }
-          });
-      });
-    },
-  },
-};
+// export default {
+//   state: {},
+//   getters: {},
+//   mutations: {},
+//   actions: {
+//     login: ({ c ommit }, { email, password }) => {
+//       return new Promise((resolve, reject) => {
+//         axios
+//           .post("api login route goes here", { email, password })
+//           .then(({ data, status }) => {
+//             if (status === 200) {
+//               resolve(true);
+//             }
+//           })
+//           .catch((error) => {
+//             reject(error);
+//           });
+//       });
+//     },
+//     register: ({ commit }, { username, email, password }) => {
+//       return new Promise((resolve, reject) => {
+//         axios
+//           .post("api register router goes here", {
+//             username,
+//             email,
+//             password,
+//           })
+//           .then(({ data, status }) => {
+//             if (status === 201) {
+//               resolve(true);
+//             }
+//           });
+//       });
+//     },
+//   },
+// };
